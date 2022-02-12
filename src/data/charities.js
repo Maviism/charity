9e4803c5b858261cec7d6c47c6968a3040048342
Muadz Izh <muadzihharul@gmail.com>
2022-02-12 11:05:20 +0300

    changing html template to  svelte

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title : 'Help For School',
+            category : 'Money',
+            thumbnail : 'https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2021/04/931/524/Generic-Classroom.jpg?ve=1&tl=1',
+            pledged: 99000,
+            target: 100000,
+            date_end: +new Date('20 February 2022'),
+            profile_photo: 'https://www.seekpng.com/png/detail/60-604032_face-businessman-png-dummy-images-for-testimonials.png',
+            profile_name: 'Paulo Julio',
+            no_pledges: 0
+        }
+    ]
+}
---
+++ src/pages/Home.svelte
@@ -0,0 +1,29 @@
+<script>
+	import CharityList from '../components/CharityList.svelte';
+	import Header from '../components/Header.svelte';
+	import Welcome from '../components/Welcome.svelte';
+	import Promo from '../components/Promo.svelte';
+	import Footer from '../components/Footer.svelte';
+
+	import { charities } from '../data/charities';
+	
+	let title = "Charity";
+	setTimeout(() => {
+		title = "Donasi";
+	}, 2000);
+
+</script>
+
+
+<style>
+	
+
+</style>
+
+<div>
+	<Header/>
+	<Welcome/>
+	<CharityList {charities}/>
+	<Promo />
+	<Footer />
+</div>
